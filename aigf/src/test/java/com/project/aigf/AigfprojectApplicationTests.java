d34d9098ac6ecdb2617a798b8f4e8b42185d7bdd
dhingl <87662284+dhingl@users.noreply.github.com>
2025-02-16 16:36:44 +0530

    Initial commit

---
+++ aigf/src/test/java/com/project/aigf/AigfprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aigf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigfprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
